4770d406f6db13c3a427ff0a3e7235d9a53e1667
enotq <ahmadaqeelknx@gmail.com>
2025-09-24 00:30:38 +0700

    ti3

--- kickoff_standoff/settings.py
+++ kickoff_standoff/settings.py
@@ -32,6 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-aqeel-kickoffstandoff.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://ahmad-aqeel-kickoffstandoff.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'kickoff_standoff.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
